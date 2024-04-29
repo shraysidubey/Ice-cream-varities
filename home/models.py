11ffbb928b8ede4a0d55d1d197375bcb5a3b7c12
Shraysi Dubey <65558817+shraysidubey@users.noreply.github.com>
2024-04-29 13:43:30 +0530

    ice cream services site

---
+++ home/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Contact(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
+    phone = models.CharField(max_length=120)
+    desc = models.TextField()
+    date = models.DateField()
